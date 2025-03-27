c5aac805ba2d3d94016314a223db58c71e07b9a2
chinazes <fergerhong7@gmail.com>
2025-03-27 22:52:34 +0300

    добавлен вывод сообщения в дополнительной ветке

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,6 +15,17 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            label1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
         }
     }
 }
